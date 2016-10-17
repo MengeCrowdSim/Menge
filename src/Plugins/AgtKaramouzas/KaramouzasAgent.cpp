d71488276b7ff1c826cc78c529908806dd4ee83b
Sean Curtis <Sean Curtis>
2016-10-16 20:15:47 -0700

    Patch karamouzas model -- missing std::max

--- src/Plugins/AgtKaramouzas/KaramouzasAgent.cpp
+++ src/Plugins/AgtKaramouzas/KaramouzasAgent.cpp
@@ -39,6 +39,8 @@ Any questions or comments should be sent to the authors {menge,geom}@cs.unc.edu
 #include "KaramouzasAgent.h"
 #include "KaramouzasSimulator.h"
 #include "Math/geomQuery.h"
+
+#include <algorithm>
 #include <list>
 
 namespace Karamouzas {
