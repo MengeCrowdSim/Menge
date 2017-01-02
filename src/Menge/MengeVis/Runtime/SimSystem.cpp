9111baa7d735664c5e696f129f9283f68110b4ce
Sean Curtis <Sean Curtis>
2017-01-02 15:41:50 -0800

    [WIP] Adding MengeVis to the project
    MengeVis does *not* build.  There is still a great deal to do to it to
    get it to build.  Things that still need to be done.
    
    1) MengeCore needs to have *all* header files modified to have full
       $SrcDir relative path names (e.g., MengeCore/Agents/thing.h
       a) Modify project
       b) Modify *all* header references.
    2) Continue passing through MengeVis (with appropriately compliant core
       header relatinoships.

---
+++ src/Menge/MengeVis/Runtime/SimSystem.cpp
@@ -0,0 +1,238 @@
+/*
+
+License
+
+Menge
+Copyright © and trademark ™ 2012-14 University of North Carolina at Chapel Hill. 
+All rights reserved.
+
+Permission to use, copy, modify, and distribute this software and its documentation 
+for educational, research, and non-profit purposes, without fee, and without a 
+written agreement is hereby granted, provided that the above copyright notice, 
+this paragraph, and the following four paragraphs appear in all copies.
+
+This software program and documentation are copyrighted by the University of North 
+Carolina at Chapel Hill. The software program and documentation are supplied "as is," 
+without any accompanying services from the University of North Carolina at Chapel 
+Hill or the authors. The University of North Carolina at Chapel Hill and the 
+authors do not warrant that the operation of the program will be uninterrupted 
+or error-free. The end-user understands that the program was developed for research 
+purposes and is advised not to rely exclusively on the program for any reason.
+
+IN NO EVENT SHALL THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL OR THE AUTHORS 
+BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS 
+DOCUMENTATION, EVEN IF THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL OR THE 
+AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL AND THE AUTHORS SPECIFICALLY 
+DISCLAIM ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE AND ANY STATUTORY WARRANTY 
+OF NON-INFRINGEMENT. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND 
+THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL AND THE AUTHORS HAVE NO OBLIGATIONS 
+TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+Any questions or comments should be sent to the authors {menge,geom}@cs.unc.edu
+
+*/
+
+#include "MengeVis/Runtime/SimSystem.h"
+
+#include "MengeCore/Agents/Obstacle.h"
+#include "MengeCore/Agents/SCBWriter.h"
+#include "MengeCore/Agents/SimulatorInterface.h"
+#include "MengeCore/Agents/SpatialQueries/SpatialQuery.h"
+#include "MengeCore/BFSM/FSM.h"
+#include "MengeVis/Runtime/VisAgent.h"
+#include "MengeVis/Runtime/VisObstacle.h"
+#include "MengeVis/SceneGraph/GLScene.h"
+
+#include <set>
+
+namespace MengeVis {
+
+	using Menge::Agents::BaseAgent;
+	using Menge::Agents::Obstacle;
+	using Menge::Agents::SCBWriter;
+	using Menge::Agents::SCBFileException;
+	using Menge::Agents::SimulatorInterface;
+	using Menge::BFSM::FSM;
+	using Menge::BFSM::FSMFatalException;
+	using Menge::Math::Vector2;
+	using Menge::Math::Vector3;
+	using SceneGraph::SystemStopException;
+	using SceneGraph::GLScene;
+
+	////////////////////////////////////////////////////////////////////////////
+	//			Implementation of SimSystem
+	////////////////////////////////////////////////////////////////////////////
+
+	SimSystem::SimSystem( bool visualize ): SceneGraph::System(), _forVis(visualize), _sim(0x0),
+											_fsm(0x0), _scbWriter(0x0),_lastUpdate(0.f),
+											_isRunning(true), _maxDuration(100.f) {
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	SimSystem::SimSystem( bool visualize, float duration ): SceneGraph::System(),
+															_forVis(visualize), _sim(0x0),
+															_fsm(0x0), _scbWriter(0x0),
+															_lastUpdate(0.f), _isRunning(true),
+															_maxDuration(duration) {
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	SimSystem::~SimSystem() {
+			if ( _sim ) delete _sim;
+			if ( _fsm ) delete _fsm;
+			if ( _scbWriter ) delete _scbWriter;
+		}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	bool SimSystem::updateScene( float time ) {
+		const int agtCount = static_cast<int>( _sim->getNumAgents() );
+		if ( _isRunning ) {
+			if ( _scbWriter ) _scbWriter->writeFrame( _fsm );	
+			_lastUpdate = _sim->getGlobalTime();
+			if ( _lastUpdate > _maxDuration ) {
+				_isRunning = false;
+			} else {
+				for ( size_t i = 0; i <= _sim->getSubSteps(); ++i ) {
+					try {
+						_isRunning = !_fsm->doStep();
+					} catch ( FSMFatalException & e ) {
+						logger << Logger::ERR_MSG << "Error in updating the finite state machine";
+						logger << " -- stopping!\n";
+						logger << "\t" << e.what() << "\n";
+						throw SystemStopException();
+					}
+
+					_sim->doStep();
+					if ( _forVis ) {
+						updateAgentPosition( agtCount );
+					}
+					try {
+						_fsm->doTasks();
+					} catch ( FSMFatalException &e ) {
+						logger << Logger::ERR_MSG << e.what() << "\n";
+						throw SystemStopException();
+					}
+				}
+			}
+		}
+		if ( !_isRunning ) {
+			// TODO: WHy is this here??
+			throw SystemStopException();
+			return false;
+		}
+		return true;
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	bool SimSystem::isFinished() const { 
+		return _fsm->allFinal(); 
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	void SimSystem::setSimulator( SimulatorInterface * sim, FSM * fsm ) {
+		if ( _sim ) {
+			std::string msg( "Simulator already assigned to SimSystem" );
+			throw SimSystemFatalException( msg );
+		}
+		_sim = sim;
+		_fsm = fsm;
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	void SimSystem::setSimulator( SimulatorInterface * sim, FSM * fsm, 
+								  const std::string & outFileName, 
+								  const std::string & scbVersion ) {
+		setSimulator( sim, fsm );
+		try {
+			_scbWriter = new SCBWriter( outFileName, scbVersion, sim );
+		} catch ( SCBFileException ) {
+			std::string msg( "Unable to create SCB file: ");
+			msg += outFileName;
+			throw SimSystemFatalException( msg );
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	void SimSystem::addObstacleToScene( GLScene * scene ) {
+			// TODO: If the bsptree (ObstacleKDTree.h) chops up the obstacles, this isn't doing the
+			//		right thing.  Currently, the bsptree chops them
+			//	THIS IS A HACK to address the issues of the ObstacleKDTree
+			//		The right thing to do is modify things so that they are not chopped up.
+			std::set< const Obstacle * > handled;
+			const std::vector< Obstacle * > & obstacles = _sim->getSpatialQuery()->getObstacles();
+			for ( size_t o = 0; o < obstacles.size(); ++o ) {
+				
+				const Obstacle * obst = obstacles[ o ];
+				if ( handled.find( obst ) == handled.end() ) {
+					Vector2 p0a = obst->getP0();
+					Vector2 p1a = obst->getP1();
+					const Obstacle * next = obst->_nextObstacle;
+					while ( next && next->_unitDir * obst->_unitDir >= 0.99999f ) {
+						handled.insert( next );
+						p1a.set( next->getP1() );
+						next = next->_nextObstacle;
+					}
+					Vector3 p0( p0a.x(), _sim->getElevation( p0a ), p0a.y() );
+					Vector3 p1( p1a.x(), _sim->getElevation( p1a ), p1a.y() );
+					VisObstacle * vo = new VisObstacle( p0, p1 );
+					
+					scene->addNode( vo );
+					handled.insert( obst );
+				}
+			}
+		}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	void SimSystem::addAgentsToScene( GLScene * scene ) {
+		_visAgents = new VisAgent * [ _sim->getNumAgents() ];
+		for ( size_t a = 0; a < _sim->getNumAgents(); ++a ) {
+			BaseAgent * agt =  _sim->getAgent( a );
+			VisAgent * agtNode = new VisAgent( agt );
+			float h = _sim->getElevation( agt );
+			agtNode->setPosition( agt->_pos.x(), h, agt->_pos.y() );
+			scene->addNode( agtNode );
+			_visAgents[ a ] = agtNode;
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	void SimSystem::populateScene( GLScene * scene ) {
+		assert( _sim !=  0x0 && "Can't add SimSystem to scene when no simulator is connected" );
+
+		addAgentsToScene( scene );
+		addObstacleToScene( scene );
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	void SimSystem::updateAgentPosition( int agtCount ) {
+		#pragma omp parallel for
+		for ( int a = 0; a < agtCount; ++a ) {
+			const BaseAgent * agt = _visAgents[ a ]->getAgent();
+			float h = _sim->getElevation( agt );
+			_visAgents[ a ]->setPosition( agt->_pos.x(), h, agt->_pos.y() );
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	size_t SimSystem::getAgentCount() const { 
+		return _sim->getNumAgents(); 
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+}	// namespace MengeVis
