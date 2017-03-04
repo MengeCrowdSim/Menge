416c206d2d562339bc19bd2a906224ff4b174321
Sean Curtis <Sean Curtis>
2017-03-04 11:22:15 -0800

    Add new license data to plugins

--- src/Plugins/StressGas/BaseStressAction.h
+++ src/Plugins/StressGas/BaseStressAction.h
@@ -1,8 +1,20 @@
-/*!
- *	@file		BaseStressAction.h
- *	@brief		Definition of the basic Stress action -- all actions which accumualte stress come 
- *				through this base class.
- */
+/*
+ Menge Crowd Simulation Framework
+
+ Copyright and trademark 2012-17 University of North Carolina at Chapel Hill
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+ or
+    LICENSE.txt in the root of the Menge repository.
+
+ Any questions or comments should be sent to the authors menge@cs.unc.edu
+
+ <http://gamma.cs.unc.edu/Menge/>
+*/
 #ifndef __BASE_STRESS_ACTION_H__
 #define __BASE_STRESS_ACTION_H__
 
--- src/Plugins/StressGas/TimedStressAction.h
+++ src/Plugins/StressGas/TimedStressAction.h
@@ -1,7 +1,20 @@
-/*!
- *	@file		TimedStressAction.h
- *	@brief		Definition of actions which triggers accumulation of stress based on elapsed time.
- */
+/*
+ Menge Crowd Simulation Framework
+
+ Copyright and trademark 2012-17 University of North Carolina at Chapel Hill
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+ or
+    LICENSE.txt in the root of the Menge repository.
+
+ Any questions or comments should be sent to the authors menge@cs.unc.edu
+
+ <http://gamma.cs.unc.edu/Menge/>
+*/
 #ifndef __TIMED_STRESS_ACTION_H__
 #define __TIMED_STRESS_ACTION_H__
 
