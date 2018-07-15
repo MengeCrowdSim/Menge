2b2efd663d6acd45c7a4d5a59b4a0ad33f801cc0
Maurício Borges Silva <mauricioborges@gmail.com>
2018-07-15 19:29:08 -0300

    cleaning up and commenting test file

--- src/test/MengeCore/first.cpp
+++ src/test/MengeCore/first.cpp
@@ -1,33 +1,16 @@
-#include <iostream>
-#include <MengeVis/Runtime/SimSystem.h>
 #include "gtest/gtest.h"
-#include "gmock/gmock.h"
 #include "MengeCore/Runtime/SimulatorDB.h"
 #include "MengeCore/PluginEngine/CorePluginEngine.h"
-#include "MengeVis/SceneGraph/GLScene.h"
 
+
+// Tests created heavily based on Michael Feathers purpose for testing systems you are not used to
+// through characterization tests: https://michaelfeathers.silvrback.com/characterization-testing
 
 using namespace Menge;
 using Menge::PluginEngine::CorePluginEngine;
-using Menge::Agents::SimulatorInterface;
 
-using MengeVis::Runtime::SimSystem;
-using MengeVis::SceneGraph::GLDagNode;
-
-using MengeVis::Runtime::SimSystem;
-
-
-using ::testing::AtLeast;
-using ::testing::_;
-
-class MockScene : public MengeVis::SceneGraph::GLScene {
-public:
-    MOCK_METHOD2(addNode, bool(MengeVis::SceneGraph::GLNode * node, MengeVis::SceneGraph::GLDagNode * parent));
-
-
-};
-
-TEST(CharacterizingCorePluginEngine, defaultSimulationEngines) {
+// Creates instance of plugins engine with empty SimulatorDB object to verify default models
+TEST(CharacterizingCorePluginEngine, shouldUseOrcaAndPedVOAsDefaultModels) {
     SimulatorDB simDB;
     CorePluginEngine plugins(&simDB);
     EXPECT_EQ(simDB.modelCount(), 2);
